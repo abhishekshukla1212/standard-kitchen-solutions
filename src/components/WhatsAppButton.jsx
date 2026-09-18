1b8232858b08230d4eb079ed6ed07bc8227caa33
Abhishek Shukla <abhishek.s.shukla@slrtce.in>
2026-09-18 21:38:37 +0530

    Update website

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -3,12 +3,12 @@ import { FaWhatsapp } from "react-icons/fa";
 function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/919999999999"
+      href="https://wa.me/917304252693"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-green-500 text-white p-4 rounded-full shadow-lg hover:scale-110 transition duration-300"
     >
-      <FaWhatsapp size={32} />
+      <FaWhatsapp size={30} />
     </a>
   );
 }
